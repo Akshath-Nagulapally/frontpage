1c32708a38c51a0c9dc1aec7d9994e68bd5448e8
Akshath-Nagulapally <akshathnag04@gmail.com>
2023-08-21 22:51:26 +0530

    removed logo

--- pages/index.js
+++ pages/index.js
@@ -374,58 +374,6 @@ export default function Home({ publications }) {
           <div className="container relative flex items-center mx-auto">
             {/* Logo */}
             <div className="z-50 sm:w-8 sm:h-8 w-9 h-9 flex items-center">
-              <svg
-                id="b613d120-e911-4f71-b7bc-d9b9e1bbdc6f"
-                data-name="Layer 1"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 93.13 75.2"
-              >
-                <rect
-                  className="fill-current text-brand"
-                  x="-3.43"
-                  y="39.29"
-                  width="32.19"
-                  height="8.78"
-                  rx="4.39"
-                  transform="translate(-27.18 21.75) rotate(-45)"
-                />
-                <rect
-                  className="fill-current text-brand"
-                  x="-3.43"
-                  y="22.74"
-                  width="32.19"
-                  height="8.78"
-                  rx="4.39"
-                  transform="translate(22.89 -1.01) rotate(45)"
-                />
-                <rect
-                  className="fill-current text-brand"
-                  x="64.37"
-                  y="22.74"
-                  width="32.19"
-                  height="8.78"
-                  rx="4.39"
-                  transform="translate(156.55 -10.59) rotate(135)"
-                />
-                <rect
-                  className="fill-current text-brand"
-                  x="64.37"
-                  y="39.29"
-                  width="32.19"
-                  height="8.78"
-                  rx="4.39"
-                  transform="translate(106.48 131.47) rotate(-135)"
-                />
-                <rect
-                  className="fill-current text-brand"
-                  x="41.93"
-                  y="-1.17"
-                  width="8.78"
-                  height="77.54"
-                  rx="4.39"
-                  transform="translate(11.31 -10.71) rotate(15)"
-                />
-              </svg>
             </div>
             {/* Text */}
             <div className="flex items-center ml-4">
